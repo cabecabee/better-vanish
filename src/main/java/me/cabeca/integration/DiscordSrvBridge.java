e96821f2f2d507ce99fe9a68b42de9709011efdc
cabecabee <bernardomb004@gmail.com>
2026-07-02 19:57:35 -0300

    aaaaaaaaaa

--- src/main/java/me/cabeca/integration/DiscordSrvBridge.java
+++ src/main/java/me/cabeca/integration/DiscordSrvBridge.java
@@ -1,12 +1,17 @@
 package me.cabeca.integration;
 
 import github.scarsz.discordsrv.DiscordSRV;
+import github.scarsz.discordsrv.dependencies.jda.api.EmbedBuilder;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.TextChannel;
-import github.scarsz.discordsrv.util.DiscordUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public final class DiscordSrvBridge {
+
+    private static String avatarUrl(Player player) {
+        String uuidNoDash = player.getUniqueId().toString().replace("-", "");
+        return "https://minotar.net/helm/" + uuidNoDash + "/128";
+    }
 
     public static void sendFakeLeave(Player player) {
         if (!Bukkit.getPluginManager().isPluginEnabled("DiscordSRV")) return;
@@ -14,7 +19,11 @@ public final class DiscordSrvBridge {
         TextChannel channel = DiscordSRV.getPlugin().getMainTextChannel();
         if (channel == null) return;
 
-        DiscordUtil.sendMessage(channel, "⬅️ **" + player.getName() + "** saiu do servidor.");
+        EmbedBuilder eb = new EmbedBuilder()
+                .setColor(0xFF0000)
+                .setAuthor(player.getName() + " left the server", null, avatarUrl(player));
+
+        channel.sendMessageEmbeds(eb.build()).queue();
     }
 
     public static void sendFakeJoin(Player player) {
@@ -23,6 +32,9 @@ public final class DiscordSrvBridge {
         TextChannel channel = DiscordSRV.getPlugin().getMainTextChannel();
         if (channel == null) return;
 
-        DiscordUtil.sendMessage(channel, "➡️ **" + player.getName() + "** entrou no servidor.");
+        EmbedBuilder eb = new EmbedBuilder()
+                .setColor(0x00FF00)
+                .setAuthor(player.getName() + " joined the server", null, avatarUrl(player));
+        channel.sendMessageEmbeds(eb.build()).queue();
     }
 }
